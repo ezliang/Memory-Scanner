df723b4f29e2e59860f9c0a9fed60e3407e2d18f
ezliang <eliang@nyu.edu>
2016-09-30 02:18:24 -0400

    GetModSize

--- MemoryScanner/scanner.cpp
+++ MemoryScanner/scanner.cpp
@@ -4,10 +4,13 @@
 #include <winnt.h>
 #include <stdlib.h>	
 #include <stdio.h>
+#include <iostream>
 #include "error.h"
 
+#define MakePtr( cast, ptr, addValue ) (cast)( (DWORD_PTR)(ptr) + (DWORD_PTR)(addValue))
+
 unsigned long GetBaseAddress(HANDLE proc);
-DWORD GetModSize(HMODULE mod_base);
+DWORD GetModSize(HANDLE proc, HMODULE mod_base);
 
 int main(int argc, char** argv){
 
@@ -17,6 +20,7 @@ int main(int argc, char** argv){
 	} 
 
 	unsigned long addr = 0;
+	unsigned long mod_end;
 	DWORD pid = strtol(argv[1], NULL, 10);
 	DWORD mod_size = 0;
 	MEMORY_BASIC_INFORMATION mbi;
@@ -29,16 +33,18 @@ int main(int argc, char** argv){
 
 	addr = GetBaseAddress(proc);
 
-	mod_size = GetModSize((HMODULE)addr);
-	
+	mod_size = GetModSize(proc,(HMODULE)addr);
+	printf("Mod size 0x%x\n", mod_size);
+	mod_end = addr + mod_size;
 
 	printf("Loaded at %p\n", addr);
 
-	VirtualQueryEx(proc, (void*)addr, &mbi, sizeof(MEMORY_BASIC_INFORMATION));
-		
+	for (unsigned long i = addr; i < mod_end;){
+		VirtualQueryEx(proc, (void*)i, &mbi, sizeof(MEMORY_BASIC_INFORMATION));
+
 		printf("Base Address: 0x%p\nAllocation Base: %p AllocationProtect: %d\nRegion Size: %u\nState: %d\nProtect: %d\nType: %d\n\n", mbi.BaseAddress, mbi.AllocationBase, mbi.AllocationProtect, mbi.RegionSize, mbi.State, mbi.Protect, mbi.Type);
-		addr += mbi.RegionSize;
-	
+		i += mbi.RegionSize;
+	}
 	
 
 	CloseHandle(proc);
@@ -52,7 +58,7 @@ unsigned long GetBaseAddress(HANDLE proc){
 
 	GetModuleFileNameExA(proc, NULL, proc_name, MAX_PATH);
 
-	if (c = strrchr(proc_name, '\\')){
+	if (c = strrchr(proc_name, '\\')) {
 		strcpy_s(proc_name, c+1);
 	}
 
@@ -61,13 +67,10 @@ unsigned long GetBaseAddress(HANDLE proc){
 		for (size_t i = 0; i < bytes_read / sizeof(HMODULE); ++i){
 
 			char mod_name[MAX_PATH];
-			if (GetModuleBaseNameA(proc, hmods[i], mod_name, sizeof(mod_name))){
+			if (GetModuleBaseNameA(proc, hmods[i], mod_name, sizeof(mod_name))) {
 				if (!strcmp(mod_name,proc_name)){
 					return (unsigned long) hmods[i];
 				}
-			}
-			else{
-				ExitProcess();
 			}
 		}
 	}
@@ -76,7 +79,25 @@ unsigned long GetBaseAddress(HANDLE proc){
 	return NULL;
 }
 
-DWORD GetModSize(HMODULE mod_base){
+DWORD GetModSize(HANDLE proc, HMODULE mod_base){
+	IMAGE_DOS_HEADER dos_head;
+	IMAGE_NT_HEADERS nt_head;
+
+	if (!mod_base)
+		return NULL;
+
+	if (!ReadProcessMemory(proc, mod_base, &dos_head, sizeof(IMAGE_DOS_HEADER), NULL))
+		return NULL;
 	
+	if (dos_head.e_magic != IMAGE_DOS_SIGNATURE)
+		return NULL;
+
+	if (!ReadProcessMemory(proc, MakePtr(LPCVOID, mod_base, dos_head.e_lfanew), &nt_head, sizeof(IMAGE_NT_HEADERS), NULL))
+		return NULL;
+	
+	if (nt_head.Signature != IMAGE_NT_SIGNATURE)
+		return NULL;
+	
+	return nt_head.OptionalHeader.SizeOfImage;
 
 }
